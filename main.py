2a5aa413a09ccaa5f4558eeb6b906d2be32685d2
kvjrk <kont530@mail.ru>
2024-05-29 19:31:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,32 @@ win_height = 500
 window = display.set_mode((win_width, win_height))
 window.fill(back)
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Player(GameSprite):
+    key_pressed = key.get_pressed()
+    def update_l(self):
+        keys = key.get_pressed()
+        if key_pressed[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key_pressed[K_DOWN] and self.rect.y < 630:
+            self.rect.y += self.speed
+    def update_r(self):
+        keys = key.get_pressed()
+        if key_pressed[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if key_pressed[K_s] and self.rect.y < 630:
+            self.rect.y += self.speed
+
 game = True
 finish = False 
 clock = time.Clock()
