adc7e0a9750b043ab9923c7740c27ec5ebda9d21
kvjrk <kont530@mail.ru>
2024-05-30 15:46:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,7 @@ while game:
             finish = True
             window.blit(lose2, (200, 200))
 
-        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket1, ball):
+        if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
             speed_x *= -1
             speed_y *= -1
         
